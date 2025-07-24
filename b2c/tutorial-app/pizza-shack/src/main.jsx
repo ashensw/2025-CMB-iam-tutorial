0b57e1421eba466b4e62973275a707dfe86f8161
Brion <info@brionmario.com>
2025-07-24 12:39:01 +0530

    refactor: update authentication library and refactor App component
    - Replaced @asgardeo/auth-react with @asgardeo/react in package.json.
    - Refactored App component to use SignedIn and SignedOut components for authentication state management.
    - Simplified user dropdown and login button handling using UserDropdown and SignInButton components.
    - Updated main.jsx to use AsgardeoProvider for authentication configuration.

--- b2c/tutorial-app/pizza-shack/src/main.jsx
+++ b2c/tutorial-app/pizza-shack/src/main.jsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from '@asgardeo/auth-react'
+import { AsgardeoProvider } from '@asgardeo/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider
-      config={ {
-        signInRedirectURL: 'http://localhost:5173',
-        signOutRedirectURL: 'http://localhost:5173',
-        clientID: 'Rxzf4BLJUao3_Fr5nq3bWiVmPYEa',
-        baseUrl: 'https://api.asgardeo.io/t/wso2conasia',
-        scope: ['openid', 'profile'],
-      } }
+    <AsgardeoProvider
+      clientId='Rxzf4BLJUao3_Fr5nq3bWiVmPYEa'
+      baseUrl='https://api.asgardeo.io/t/wso2conasia'
+      preferences={{
+        theme: {
+          mode: 'light',
+        }
+      }}
     >
       <App />
-    </AuthProvider>
+    </AsgardeoProvider>
   </StrictMode>,
 )
